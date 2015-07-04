0caa487b8560b4f13324f23e8e353bfa1fe3149f
Nelma Perera <npererar@utexas.edu>
2015-07-03 20:07:31 -0500

    Create travis.bash

---
+++ travis.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_file () {
+    if [ -f "$1" ]
+    then
+        echo "$1 found"
+    else
+        echo "$1 not found"
+        exit -1
+    fi
+}
+
+echo "Checking for all files"
+
+check_file "apiary.apib"
+check_file "README.md"
