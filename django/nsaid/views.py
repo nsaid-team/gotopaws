efbfc481fa88177f722aeea23665e540b5b579b9
User <alexander.adams.mail@gmail.com>
2015-07-04 22:28:08 +0000

    Updated Django to accept new html files

--- django/nsaid/views.py
+++ django/nsaid/views.py
@@ -7,13 +7,21 @@ def test(request):
     return HttpResponse(html)
 
 def home(request):
-    template = loader.get_template('testInput.html')
+    template = loader.get_template('Home.html')
     return HttpResponse(template.render())
 
 def about(request):
     template = loader.get_template('About.html')
     return HttpResponse(template.render())
 
-def testInput(request):
-    template = loader.get_template('testInput.html')
+def cats(request):
+    template = loader.get_template('Cats.html')
+    return HttpResponse(template.render())
+
+def dogs(request):
+    template = loader.get_template('Dogs.html')
+    return HttpResponse(template.render())
+
+def shelters(request):
+    template = loader.get_template('Shelters.html')
     return HttpResponse(template.render())
