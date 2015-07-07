297a032327d3a15be4420a8b0e894bba1ff1dea6
Sam Morris <sfmorris@gmail.com>
2015-07-07 14:28:55 -0500

    reordering imports to see if the django.utils or django.test modules are not installed too

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 # -------
 
 import requests, json
-from django.test.utils import setup_test_environment
-from django.test import TestCase
 from django.utils import unittest
+from django.test import TestCase
+from django.test.utils import setup_test_environment
 from models import Pet, Shelter, City
 
 # -----------
