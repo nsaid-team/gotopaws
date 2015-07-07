91e86913a299ea3c6089d6c2d70d5de8048c3907
nelmiux <npererar@utexas.edu>
2015-07-07 18:09:40 -0500

    Testing

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'models.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'nsaid.settings'
 
     from django.core.management import execute_from_command_line
 
