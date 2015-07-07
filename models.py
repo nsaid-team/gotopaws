44fa683d168e1e27644ff1540cdf0ea48fe74afa
Nelma Perera <npererar@utexas.edu>
2015-07-07 18:33:05 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,26 +1,24 @@
 from django.db import models
 
-    # -------------
-    # Pet_model
-    # -------------
+    # ------------
+    # City_model
+    # ------------
 
-class Pet(models.Model):
+class City(models.Model):
     """
-    The model contains a petsid id string, name, age, size, breed, shelter, city, and a pic_url
-    The __str__ method is used to return the petid identifier.
+    The model contains a name, state, country, and vet_url 
+    The list of pets in a city is handled by Pet having a foreign key Pet.city.
+    The list of shelters in a city is handled by Shelter having a foreign key Shelter.city. 
+    The __str__ method is used to return the concatenated city and state.
     """
 
-    petsid = models.CharField(primary_key=True)
     name = models.CharField(max_length=100)
-    age = models.CharField(max_length=20)
-    size = models.CharField(max_length=20)
-    breed = models.CharField(max_length=100)
-    shelter = models.ForeignKey(Shelter)
-    city = models.ForeignKey(City)
-    pic_url = models.CharField(max_length=500)
-    
+    state = models.CharField(max_length=300)
+    country = models.CharField(max_length=100)
+    vet_url = models.CharField(max_length=500)
+
     def __str__ (self):
-        return self.petsid
+        return self.name + " " + self.state
 
 
     # ------------
@@ -46,22 +44,25 @@ class Shelter(models.Model):
     def __str__ (self):
         return self.shelterid
 
-    # ------------
-    # City_model
-    # ------------
 
-class City(models.Model):
+    # -------------
+    # Pet_model
+    # -------------
+
+class Pet(models.Model):
     """
-    The model contains a name, state, country, and vet_url 
-    The list of pets in a city is handled by Pet having a foreign key Pet.city.
-    The list of shelters in a city is handled by Shelter having a foreign key Shelter.city. 
-    The __str__ method is used to return the concatenated city and state.
+    The model contains a petsid id string, name, age, size, breed, shelter, city, and a pic_url
+    The __str__ method is used to return the petid identifier.
     """
 
+    petsid = models.CharField(primary_key=True)
     name = models.CharField(max_length=100)
-    state = models.CharField(max_length=300)
-    country = models.CharField(max_length=100)
-    vet_url = models.CharField(max_length=500)
-
+    age = models.CharField(max_length=20)
+    size = models.CharField(max_length=20)
+    breed = models.CharField(max_length=100)
+    shelter = models.ForeignKey(Shelter)
+    city = models.ForeignKey(City)
+    pic_url = models.CharField(max_length=500)
+    
     def __str__ (self):
-        return self.name + " " + self.state
+        return self.petsid
