5999d3185473fc315fb6a96ef103191ad08ba579
Nelma Perera <npererar@utexas.edu>
2015-07-07 16:25:45 -0500

    Update wsgi.py

--- django/nsaid/wsgi.py
+++ django/nsaid/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsaid.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nsaid.settings'
 
 application = get_wsgi_application()
