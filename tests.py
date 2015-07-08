1898818f499d9c3709af60d07ab3aa91e7aa1d00
Nelma Perera <npererar@utexas.edu>
2015-07-07 23:54:20 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -71,7 +71,6 @@ class Test (unittest.TestCase) :
             shelterid = "TX1218",
             name = "Austin Pets Alive!",
             address = "1156 West Cesar Chavez",
-            city = "Austin",
             state = "TX",
             phone = "512 961 6519",
             email = "adopt@austinpetsalive.org",
@@ -81,7 +80,6 @@ class Test (unittest.TestCase) :
         self.assertEqual(s.shelterid, "TX1218")
         self.assertEqual(s.name, "Austin Pets Alive!")
         self.assertEqual(s.address, "1156 West Cesar Chavez")
-        self.assertEqual(s.city, "Austin")
         self.assertEqual(s.state, "TX")
         self.assertEqual(s.phone, "512 961 6519")
         self.assertEqual(s.email, "adopt@austinpetsalive.org")
@@ -93,7 +91,6 @@ class Test (unittest.TestCase) :
             shelterid = "TX500",
             name = "Hound Rescue",
             address = "Lakeline Petsmart 11066 Pecan Park Blvd, Cedar Park",
-            city = "Austin",
             state = "TX",
             phone = "512 636 8473",
             email = "hound@dogs.org",
@@ -103,7 +100,6 @@ class Test (unittest.TestCase) :
         self.assertEqual(s.shelterid, "TX500")
         self.assertEqual(s.name, "Hound Rescue")
         self.assertEqual(s.address, "Lakeline Petsmart 11066 Pecan Park Blvd, Cedar Park")
-        self.assertEqual(s.city, "Austin")
         self.assertEqual(s.state, "TX")
         self.assertEqual(s.phone, "512 636 8473")
         self.assertEqual(s.email, "hound@dogs.org")
@@ -115,7 +111,6 @@ class Test (unittest.TestCase) :
             shelterid = "TX514",
             name = "Austin Animal Center",
             address = "7201 Levander Loop",
-            city = "Austin",
             state = "TX",
             phone = "512 978 0500",
             email = "hound@dogs.org",
@@ -125,7 +120,6 @@ class Test (unittest.TestCase) :
         self.assertEqual(s.shelterid, "TX500")
         self.assertEqual(s.name, "Hound Rescue")
         self.assertEqual(s.address, "Lakeline Petsmart 11066 Pecan Park Blvd, Cedar Park")
-        self.assertEqual(s.city, "Austin")
         self.assertEqual(s.state, "TX")
         self.assertEqual(s.phone, "512 636 8473")
         self.assertEqual(s.email, "animal.customerservice@austintexas.gov")
@@ -144,8 +138,6 @@ class Test (unittest.TestCase) :
             age = "Adult",
             size = "S",
             breed = "Chihuahua",
-            shelter = "TX1148",
-            city = "Austin",
             pic_url = "picurl")
 
         self.assertTrue(type(p) == Pet)
@@ -154,8 +146,6 @@ class Test (unittest.TestCase) :
         self.assertEqual(p.age, "Adult")
         self.assertEqual(p.size, "S")
         self.assertEqual(p.breed, "Chihuahua")
-        self.assertEqual(p.shelter, "TX1148")
-        self.assertEqual(p.city, "Austin")
         
     def test_pet2 (self) :
 
@@ -165,8 +155,6 @@ class Test (unittest.TestCase) :
             age = "Senior",
             size = "L",
             breed = "Applehead Siamese",
-            shelter = "TX1334",
-            city = "Austin",
             pic_url = "picurl")
 
         self.assertTrue(type(p) == Pet)
@@ -175,8 +163,6 @@ class Test (unittest.TestCase) :
         self.assertEqual(p.age, "Senior")
         self.assertEqual(p.size, "L")
         self.assertEqual(p.breed, "Applehead Siamese")
-        self.assertEqual(p.shelter, "TX1334")
-        self.assertEqual(p.city, "Austin")
     
     def test_pet3 (self) :
 
@@ -186,8 +172,6 @@ class Test (unittest.TestCase) :
             age = "Baby",
             size = "M",
             breed = "Tabby",
-            shelter = "TX1222",
-            city = "Austin",
             pic_url = "picurl")
 
         self.assertTrue(type(p) == Pet)
@@ -196,8 +180,6 @@ class Test (unittest.TestCase) :
         self.assertEqual(p.age, "Baby")
         self.assertEqual(p.size, "M")
         self.assertEqual(p.breed, "Tabby")
-        self.assertEqual(p.shelter, "TX1222")
-        self.assertEqual(p.city, "Austin")
     
     # ----
     # API Tests
