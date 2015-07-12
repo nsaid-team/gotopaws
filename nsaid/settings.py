e634d98a6884d1071c07f2a09e1c1edf4d064a2c
Nelma Perera <npererar@utexas.edu>
2015-07-12 01:04:46 -0500

    Update settings.py

--- nsaid/settings.py
+++ nsaid/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-SETTINGS_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-PROJECT_PATH = os.path.join(SETTINGS_DIR, BASE_DIR) 
-PROJECT_PATH = os.path.abspath(PROJECT_PATH)
-TEMPLATE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#SETTINGS_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+#PROJECT_PATH = os.path.join(SETTINGS_DIR, BASE_DIR) 
+#PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+#TEMPLATE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
