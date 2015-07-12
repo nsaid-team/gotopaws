b67a55250591b89ff93cf3719c58ec9367b4e4ec
alexanderadams <alexander.adams.mail@gmail.com>
2015-07-12 14:14:30 -0500

    Updated directory

--- nsaid/settings.py
+++ nsaid/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            '../html',
+            '/html',
         ],
         'APP_DIRS': False,
         'OPTIONS': {
