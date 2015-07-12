a5b4bc609042dcbc6ce8823b46bdf503e9c7d6d7
Nelma Perera <npererar@utexas.edu>
2015-07-12 00:54:44 -0500

    Update settings.py

--- nsaid/settings.py
+++ nsaid/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir) 
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
