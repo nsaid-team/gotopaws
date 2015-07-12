0090de19a69347baf0a79c54092a2982b174281a
Nelma Perera <npererar@utexas.edu>
2015-07-11 23:45:28 -0500

    Update settings.py

--- nsaid/settings.py
+++ nsaid/settings.py
@@ -62,7 +62,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=nsaid',
+    '--cover-package=City',
 ]
 
 TEMPLATES = [
