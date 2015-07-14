b69008b63a2c659a1874633d0c47ec60a136bdf8
daniela-reyes <danielareyes174@utexas.edu>
2015-07-14 02:41:42 -0500

    Update views.py

--- nsaid/views.py
+++ nsaid/views.py
@@ -15,8 +15,8 @@ def home(request):
 
 def shelters(request):
     ShelterObjects = Shelter.objects.all()
-    sheltersByAlpha = sorted([shelter.shelter_id for shelter in ShelterObjects])
-    return render_to_response("Shelters.html", sheltersByAlpha)
+    shelter_list = sorted([shelter.shelter_id for shelter in ShelterObjects])
+    return render_to_response("Shelters.html", shelter_list)
     
 """
 def shelters(request):
