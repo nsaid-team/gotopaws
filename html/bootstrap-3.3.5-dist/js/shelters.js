1fb2848211e40324d40d8ff6cde47fdc470e5a5b
daniela-reyes <danielareyes174@utexas.edu>
2015-07-13 23:55:08 -0500

    js fixes

--- html/bootstrap-3.3.5-dist/js/shelters.js
+++ html/bootstrap-3.3.5-dist/js/shelters.js
@@ -15,23 +15,26 @@ app.controller("ShelterTable",  function ($http) {
         var temp = {};
         this.loaded = true;
 
+    for (var i = 0, len = val.length; i < len; i++) {
+        temp['id']= val[i][1];
+        temp['name']= val[i][2];
+        temp['address']= val[i][3];
+        temp['city'] = val[i][4];
+        temp['state']= val[i][5];
+        temp['phone']= val[i][6];
+        temp['email']=val[i][7];
+        temp['hours']=val[i][8];
+
+        dict.data[val[i][0]] = temp;
+
+    }
+
+
   };
  
 });
 
-//   for (var i = 0, len = val.length; i < len; i++) {
-  //       temp['id']= val[i][1];
-  //       temp['name']= val[i][2];
-  //       temp['address']= val[i][3];
-  //       temp['city'] = val[i][4];
-  //       temp['state']= val[i][5];
-  //       temp['phone']= val[i][6];
-  //       temp['email']=val[i][7];
-  //       temp['hours']=val[i][8];
-
-  //       dict.data[val[i][0]] = temp;
-
-  //   }
+  
 
 /*
     $http.get("bootstrap-3.3.5-dist/js/3x3x3_shelters_dataset.json").success(function(load){
