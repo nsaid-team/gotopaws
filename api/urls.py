744143fa1203c39b48ee51659ad41b82835c3921
Nelma Perera Rodriguez <npererar@cs.utexas.edu>
2015-07-14 17:44:33 -0500

    Deleted no Needed api Folder

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    'api.views',
-    url(r'^pets/$', 'pet_list', name='pet_list'),
-    #url(r'^pets/(?P<pk>[0-9]+)$', 'pet_detail', name='pet_detail'),
-    url(r'^shelters/$', 'shelter_list', name='shelter_list'),
-    url(r'^cities/$', 'city_list', name='city_list'),
-)
