fb015abb75622cb0f9c5889048a3c7174933ff6a
icsmith <icsmith@cs.utexas.edu>
2015-07-17 17:26:38 -0500

    templateUrl inline

--- html/bootstrap-3.3.5-dist/js/templates.js
+++ html/bootstrap-3.3.5-dist/js/templates.js
@@ -4,7 +4,7 @@ var app = angular.module("GoToPaws", []);
 app.directive('navBar', function(){
   return{
     restrict: 'E',
-    templateUrl: '"bootstrap-3.3.5-dist/templates/Navbar.html"'
+    
   };
 });
 
@@ -18,7 +18,7 @@ app.directive('cityShelters', function(){
 app.directive('cityPets', function(){
   return{
     restrict: 'E',
-    templateUrl: 'static("/templates/Pet_fill.html")'
+    templateUrl: '/templates/Pet_fill.html'
   };
 });
 
