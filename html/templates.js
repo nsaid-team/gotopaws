f87d54abf405738586f2b3ce9c9bfb746e9bd3c2
icsmith <icsmith@cs.utexas.edu>
2015-07-16 19:11:31 -0500

    moved template files

---
+++ html/templates.js
@@ -0,0 +1,29 @@
+var app = angular.module("GoToPaws", []);
+
+app.directive('navBar', function(){
+  return{
+    restrict: 'E',
+    templateUrl: '"Navbar.html"'
+  };
+});
+
+app.directive('cityShelters', function(){
+  return{
+    restrict: 'E',
+    templateUrl: '{% static "templates/Shelter_fill.html" %}'
+  };
+});
+
+app.directive('cityPets', function(){
+  return{
+    restrict: 'E',
+    templateUrl: '{% static "templates/Pet_fill.html" %}'
+  };
+});
+
+app.directive('cityVets', function(){
+  return{
+    restrict: 'E',
+    templateUrl: '{% static "templates/Vet_fill.html" %}'
+  };
+});
