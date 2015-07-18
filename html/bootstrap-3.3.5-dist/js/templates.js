9b3a23676d63c3d660b8076e99eed127944da632
icsmith <icsmith@cs.utexas.edu>
2015-07-18 17:27:49 -0500

    html stuff

--- html/bootstrap-3.3.5-dist/js/templates.js
+++ html/bootstrap-3.3.5-dist/js/templates.js
@@ -56,12 +56,13 @@ var app2 = angular.module("mapModule", [], function(){});
 
   app.controller('MyCtrl', ['$scope', '$http',  function($scope, $http) {
     $scope.test = "Hola";
+    var temp = this;
     delete $http.defaults.headers.common['X-Requested-With'];
     $http.get("json").success(function(data) {
       $scope.results = data;
       console.log(data);
     }).error(function(data, status) {
-      $scope.data = data[0] || "Request failed";
+      temp.data = data[0] || "Request failed";
       $scope.status = status;
       console.log(data);
     });
