e5cd52b046bb0e0af4aad1a948011db7d7502ebf
icsmith <icsmith@cs.utexas.edu>
2015-07-18 23:11:19 -0500

    working on html

--- html/bootstrap-3.3.5-dist/js/templates.js
+++ html/bootstrap-3.3.5-dist/js/templates.js
@@ -67,13 +67,32 @@ var app2 = angular.module("mapModule", [], function(){});
       console.log(data);
     });
 
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ results[0].city_name: address }, function(results2, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results2[0].geometry.location);
-      var marker = new google.maps.Marker({
-      map: map,
-      position: results2[0].geometry.location
+
+    var geocoder = new google.maps.Geocoder();;
+    var map;
+  
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    }
+
+
+    var address = document.getElementById("results[0].city_name").value;
+    geocoder.geocode( { 'address': address}, function(results2, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results2[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
     });
+
+
 
 }]);
