64c08de1d5122b022c4f3d6c1ed3ea0209551e86
nelmiux <npererar@utexas.edu>
2015-07-18 19:37:45 -0500

    s

--- nsaid/views.py
+++ nsaid/views.py
@@ -200,7 +200,7 @@ def search (request):
     results_list = []
     for hit in rs['hits']['hits']:
         try:
-            results[hit['_id']] = hit["_source"]
+            results[int(hit['_id'])] = hit["_source"]
             results_list.append(results)
         except: 
             break
