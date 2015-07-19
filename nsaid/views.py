51c6599d3d4fb228fde3fe56bb2b41cbb4270e91
Sam Morris <sfmorris@gmail.com>
2015-07-19 00:11:51 -0500

    Update views.py

--- nsaid/views.py
+++ nsaid/views.py
@@ -227,7 +227,7 @@ def search (request):
                         }
                     ]
                 }
-            }
+            },
             "highlight" : {
                 "fields" : {
                     "*_text" : {}
