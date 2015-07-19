668930c1ef4f24f073973a97958a5cbbfe6c0f7a
nelmiux <npererar@utexas.edu>
2015-07-19 09:02:55 -0500

    s

--- nsaid/views.py
+++ nsaid/views.py
@@ -249,15 +249,11 @@ def search (request):
         if hit["_source"]['title'] not in titles_list :
             titles_list.append(hit["_source"]['title'])
             results['title'] = hit["_source"]['title']
-            if 'title' in hit['highlight']:
-                results['title'] = hit['highlight']['title'][0]
-                results['title'] = results['title'].replace("<em>", "<strong><em>")
-                results['title'] = results['title'].replace("</em>", "</em></strong>")
+            results['title'] = results['title'].replace("<em>", "<strong><em>")
+            results['title'] = results['title'].replace("</em>", "</em></strong>")
             results['subtitle'] = hit["_source"]['subtitle']
-            if 'subtitle' in hit['highlight']:
-                results['subtitle'] = hit['highlight']['subtitle'][0]
-                results['subtitle'] = results['subtitle'].replace("<em>", "<strong><em>")
-                results['subtitle'] = results['subtitle'].replace("</em>", "</em></strong>")
+            results['subtitle'] = results['subtitle'].replace("<em>", "<strong><em>")
+            results['subtitle'] = results['subtitle'].replace("</em>", "</em></strong>")
             results['url'] = hit["_source"]['url']
             if 'shelters_text' in hit['highlight']:
                 results['shelters_text'] = hit['highlight']['shelters_text'][0]
