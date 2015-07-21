a95a94a6e7dd310474f03bbc3e39d43749d39662
alexanderadams <alexander.adams.mail@gmail.com>
2015-07-21 15:18:42 -0500

    Fixed imports

--- nsaid/tests.py
+++ nsaid/tests.py
@@ -7,7 +7,7 @@
 import requests, json
 from django.test import TestCase
 from django.test.utils import *
-from django.db.connection import creation
+from django.db import connection
 from nsaid.models import *
 from unittest import TestLoader, TextTestRunner
 from io import StringIO
@@ -17,11 +17,11 @@ import coverage
 def run_unit_tests():
     setup_test_environment()
     old_name = settings.DATABASE_NAME
-    db = creation.create_test_db()
+    db = connection.creation.create_test_db()
     test_suite = TestLoader().loadTestsFromTestCase(Test)
     test_stream = StringIO()
     test_runner = TextTestRunner(stream=test_stream).run(test_suite)
-    creation.destroy_test_db(old_name)
+    connection.creation.destroy_test_db(old_name)
     teardown_test_environment()
     return {'results': test_stream.getvalue(), 'status': str(test_runner)}
 
