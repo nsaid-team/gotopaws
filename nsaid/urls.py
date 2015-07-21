afadf72ed8bf5bfbe4ffa969b851fc828f489e84
Sam Morris <sfmorris@gmail.com>
2015-07-21 06:14:52 -0500

    Update urls.py

--- nsaid/urls.py
+++ nsaid/urls.py
@@ -20,19 +20,19 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^about$', views.about),
+    url(r'^extapi$', views.external_api),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^unit_test/$', views.unit_test),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home),
     url(r'^home$', views.home),
     url(r'^pets$', views.pets),
-    url(r'^id([\d]+)$', views.pet_template),
     url(r'^shelters$', views.shelters),
-    url(r'^([A-Z][A-Z][\d]+)$', views.shelter_template),
     url(r'^cities$', views.cities),
+    url(r'^id([\d]+)$', views.pet_template),
+    url(r'^([A-Z][A-Z][\d]+)$', views.shelter_template),
     url(r'^([a-z_]+)$', views.city_template),
-    url(r'^extapi$', views.external_api),
-    url(r'^about$', views.about),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^unit_test/$', views.unit_test),
     # RESTful API
     url(r'^api/pets/$', views.pet_list),
     url(r'^api/shelters/$', views.shelter_list),
