aa49f0076f86b74011d059a2e4a9303a1dc01937
icsmith <icsmith@cs.utexas.edu>
2015-07-20 22:22:18 -0500

    working on pet_template

--- html/bootstrap-3.3.5-dist/js/templates.js
+++ html/bootstrap-3.3.5-dist/js/templates.js
@@ -21,26 +21,26 @@ app1.directive("navBar", function(){
   };
 });
 
-app.directive('cityShelters', function(){
-  return{
-    restrict: 'E',
-    templateUrl: '/bootstrap-3.3.5-dist/templates/Shelter_fill.html'
-  };
-});
+// app.directive('cityShelters', function(){
+//   return{
+//     restrict: 'E',
+//     templateUrl: '/bootstrap-3.3.5-dist/templates/Shelter_fill.html'
+//   };
+// });
 
-app.directive('cityPets', function(){
-  return{
-    restrict: 'E',
-    templateUrl: '/bootstrap-3.3.5-dist/templates/Pet_fill.html'
-  };
-});
+// app.directive('cityPets', function(){
+//   return{
+//     restrict: 'E',
+//     templateUrl: '/bootstrap-3.3.5-dist/templates/Pet_fill.html'
+//   };
+// });
 
-app.directive('cityVets', function(){
-  return{
-    restrict: 'E',
-    templateUrl: '/bootstrap-3.3.5-dist/templates/Vet_fill.html'
-  };
-});
+// app.directive('cityVets', function(){
+//   return{
+//     restrict: 'E',
+//     templateUrl: '/bootstrap-3.3.5-dist/templates/Vet_fill.html'
+//   };
+// });
 
 
 
