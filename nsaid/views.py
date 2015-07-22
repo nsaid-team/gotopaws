10154b9057238df5da47a5fc56ddfa175ea1b5ec
nelmiux <npererar@utexas.edu>
2015-07-21 20:36:35 -0500

    s

--- nsaid/views.py
+++ nsaid/views.py
@@ -233,9 +233,9 @@ def search (request):
                 results['title'] = hit['highlight']['title'][0]
                 results['title'] = results['title'].replace("<em>", "<strong><em>")
                 results['title'] = results['title'].replace("</em>", "</em></strong>")
-            results_list.append({'title':results['title'], 'text':results['text']})
+            results_list.append({'title':results['title'], 'text':results['text']}, 'url': result['url'])
     context = {"results_list": results_list} 
-    print({context})
+    #print({context})
     return render_to_response('search/search.html', context)
 
 def external_api (request) :
